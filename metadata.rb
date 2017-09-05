77fbe7826b1c2fefc8ad4e3ce867cab0c27e2337
Ahmed Hazem <ahazemm@gmail.com>
2017-09-05 15:24:00 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures rvm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 # source_url 'https://github.com/MurgaNikolay/chef-rvm'
 # issues_url 'https://github.com/MurgaNikolay/chef-rvm/issues'
-version '2.0.0'
+version '2.1.0'
 
 recipe 'chef_rvm', 'Installs all'
 recipe 'chef_rvm::rvm', 'Installs the rvm for users'
